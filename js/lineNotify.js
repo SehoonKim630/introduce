5e275a21c3bb849af89ba8dbdabe5b9a8f4bd2ec
SehoonKim630 <51529988+SehoonKim630@users.noreply.github.com>
2022-09-27 18:30:07 +0900

    ajax 테스트

--- js/lineNotify.js
+++ js/lineNotify.js
@@ -5,4 +5,19 @@ $('#submitBtn').click(function(){
   var message = $('#contactForm').find('#message').val();;
   
   alert(name + ', ' + email + ', ' + phone + ', ' + message);
+  
+  $.ajax({
+    type:'POST',
+    contentType: 'application/json',
+    url:'http://35.212.132.206:5000/line',
+    //data:JSON.stringify(data),
+    //dataType: 'text',
+    success: function(response){
+      console.log(response)
+    },
+    error : function(xtr, status, error){
+      alert(xtr +":"+status+":"+error);
+    }
+  });
+
 });
