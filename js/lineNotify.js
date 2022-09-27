ee3e78444f55bc2dd90832fbaa3a3ee780afe62c
SehoonKim630 <51529988+SehoonKim630@users.noreply.github.com>
2022-09-28 00:32:11 +0900

    415 에러발생 데이타 전송 형태 점검

--- js/lineNotify.js
+++ js/lineNotify.js
@@ -1,24 +1,23 @@
 $('#submitBtn').click(function(){
-  var name = $('#contactForm').find('#name').val();
-  var email = $('#contactForm').find('#email').val();
-  var phone = $('#contactForm').find('#phone').val();
-  var message = $('#contactForm').find('#message').val();
+  var nameDt = $('#contactForm').find('#name').val();
+  var emailDt = $('#contactForm').find('#email').val();
+  var phoneDt = $('#contactForm').find('#phone').val();
+  var messageDt = $('#contactForm').find('#message').val();
   
   alert(name + ', ' + email + ', ' + phone + ', ' + message);
   
-  var data = {}
-  data.name = $('#contactForm').find('#name').val(),
-  data.email = $('#contactForm').find('#email').val(), 
-  data.phone = $('#contactForm').find('#phone').val(), 
-  data.message = $('#contactForm').find('#message').val()
-  
-  var jsonStr = JSON.stringify(data);
+  var datas = {
+    name : nameDt,
+    email : emailDt,
+    phone : phoneDt,
+    message : messageDt
+  }
   
   $.ajax({
     type:'POST',
     contentType: 'aapplication/json; charset=utf-8',
     url:'https://www.martmanage86api.tk/yesmart-0.0.1-SNAPSHOT/line',
-    data:jsonStr,
+    data:datas,
     success: function(response){
       console.log(response)
     },
