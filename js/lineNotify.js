cfde5686d7f29821b9c2768c4eeed644aba52e62
SehoonKim630 <51529988+SehoonKim630@users.noreply.github.com>
2022-09-28 00:17:25 +0900

    크로스 오리진 설졍 변경

--- js/lineNotify.js
+++ js/lineNotify.js
@@ -19,12 +19,6 @@ $('#submitBtn').click(function(){
     contentType: 'aapplication/json; charset=utf-8',
     url:'https://www.martmanage86api.tk/yesmart-0.0.1-SNAPSHOT/line',
     data:jsonStr,
-    dataType:jsonp,
-    crossDomain: true,
-    headers: {
-      "accept": "application/json",
-      "Access-Control-Allow-Origin":"*"
-    },
     success: function(response){
       console.log(response)
     },
